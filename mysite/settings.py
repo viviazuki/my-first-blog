e3229b880017e6325d58e51ed5c699d8686bd10f
Kaito Nagasawa <a5816066@aoyama.jp>
2018-06-25 17:17:42 +0900

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2rv)_iww4@yozl92*&xki!id^(hcw8kksdtdhm#vev72w4y@&7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com','viviazuki.pythonanywhere.com']
 
 
 # Application definition
